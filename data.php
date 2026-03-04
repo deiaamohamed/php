28eacad413b3430de7d72582b6f92d5a14c89392
Deiaa Mohamed <moamn100g@gmail.com>
2026-03-04 10:20:21 +0200

    Enhance user data display and add detail view functionality

--- data.php
+++ data.php
@@ -21,28 +21,40 @@ file_put_contents("users.txt", $record, FILE_APPEND);
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<hr>
+<h3>Users</h3>
+<table border="1" cellpadding="10">
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Address</th>
+        <th>Country</th>
+        <th>Gender</th>
+        <th>Skills</th>
+        <th>Username</th>
+        <th>Password</th>
+        <th>Department</th>
+        <th>Action</th>
+    </tr>
     
-    <body>
-
     <?php
-    if($gender == "male"){
-        echo "<h1>Welcome Mr. $fname $lname</h1>";
-    }else{
-        echo "<h1>Welcome Ms. $fname $lname</h1>";
+    $lines = file(__DIR__ . "/users.txt"); 
+
+    foreach($lines as $idx=> $line) {
+        
+        $user_data = explode(",", $line); 
+        
+        echo "<tr>";
+        
+        foreach($user_data as $data) {
+            
+                echo "<td>$data</td>";
+            
+        }
+        echo "<td><a href='view.php?id=$idx'>View</a></td>";
+        echo "</tr>";
     }
     ?>
-    <p>First Name: <?php echo $fname ?></p>
-    <p>Last Name: <?php echo $lname ?></p>
-    <p>Address: <?php echo $address ?></p>
-    <p>Country: <?php echo $country ?></p>
-    <p>Gender: <?php echo $gender ?></p>
-    <p>Skills: <?php foreach($skills as $skill) { echo $skill . " "; } ?></p>
-    <p>Username: <?php echo $username ?></p>
-    <p>Password: noooo bro it's secret :0</p>
-    <p>Department: <?php echo $department ?></p>
-    </body>
+</table>
 
-</html>
 
---
+++ view.php
@@ -0,0 +1,22 @@
+<?php
+$row_number = $_GET['id'];
+
+$lines = file(__DIR__ . "/users.txt");
+
+$user_line = $lines[$row_number];
+
+$user_data = explode(",", $user_line);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <h2>User Details</h2>
+    <p>First Name: <?php echo $user_data[0]; ?></p>
+    <p>Last Name: <?php echo $user_data[1]; ?></p>
+    <p>Address: <?php echo $user_data[2]; ?></p>
+    
+    <br>
+    <a href="data.php">Back to List</a>
+</body>
+</html>

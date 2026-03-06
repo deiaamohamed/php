77781cc7a540e718745894c492511b68cb3111ba
Deiaa Mohamed <moamn100g@gmail.com>
2026-03-06 22:44:49 +0200

    Fix duplicated Delete button

--- data.php
+++ data.php
@@ -39,7 +39,6 @@ while($user = mysqli_fetch_assoc($result)) {
     echo "<td>
         <a href='view.php?id=".$user['id']."'>View</a> |
         <a href='delete.php?id=".$user['id']."'>Delete</a> |
-        <a href='delete.php?id=".$user['id']."'>Delete</a> |
         <a href='edit.php?id=".$user['id']."'>Edit</a>
     </td>";
 
